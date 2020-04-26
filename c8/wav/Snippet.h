c634271a18a9374f3f1e0d32f86f25ec881a19e5
pswcw <swpeng0831@gmail.com>
2020-04-26 19:11:33 +0800

    [chapter] Mikado Method step 2: add private data

--- c8/wav/Snippet.h
+++ c8/wav/Snippet.h
@@ -5,6 +5,15 @@ using namespace std;
 
 class Snippet {
 public:
+   Snippet(shared_ptr<FileUtil> fileUtil,
+      shared_ptr<WavDescriptor> descriptor,
+      const std::string& dest,
+      rlog::RLogChannel* channel)
+      : fileUtil_(fileUtil)
+      , descriptor_(descriptor)
+      , dest_(dest)
+      , channel_(channel) { }
+
    uint32_t dataLength(
          uint32_t samples,
          uint32_t bytesPerSample,
@@ -12,6 +21,12 @@ public:
          ) const {
       return samples * bytesPerSample * channels;
    }
+
+private:
+   shared_ptr<FileUtil> fileUtil_;
+   shared_ptr<WavDescriptor> descriptor_;
+   const string dest_;
+   rlog::RLogChannel* channel_;
 };
 
 #endif
