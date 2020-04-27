47f84aa93dbc0e6fc1272465a38f196e1f02d722
pswcw <swpeng0831@gmail.com>
2020-04-27 11:20:27 +0800

    [chapter 8] Mikado Method add writSnippets and writeSamples

--- c8/wav/Snippet.h
+++ c8/wav/Snippet.h
@@ -14,12 +14,72 @@ public:
       , dest_(dest)
       , channel_(channel) { }
 
+   void writeSnippet(
+         const string& name, istream& file, ostream& out,
+         FormatSubchunk& formatSubchunk,
+         DataChunk& dataChunk,
+         char* data
+         ) {
+      uint32_t secondsDesired{10};
+      if (formatSubchunk.bitsPerSample == 0) formatSubchunk.bitsPerSample = 8;
+      uint32_t bytesPerSample{formatSubchunk.bitsPerSample / uint32_t{8}};
+      uint32_t samplesToWrite{secondsDesired * formatSubchunk.samplesPerSecond};
+      uint32_t totalSamples{dataChunk.length / bytesPerSample};
+ 
+      samplesToWrite = min(samplesToWrite, totalSamples);
+ 
+      uint32_t totalSeconds { totalSamples / formatSubchunk.samplesPerSecond };
+      rLog(channel_, "total seconds %u ", totalSeconds);
+ 
+      dataChunk.length = dataLength(
+            samplesToWrite,
+            bytesPerSample,
+            formatSubchunk.channels);
+      out.write(reinterpret_cast<char*>(&dataChunk), sizeof(DataChunk));
+ 
+      uint32_t startingSample{
+         totalSeconds >= 10 ? 10 * formatSubchunk.samplesPerSecond : 0};
+ 
+      writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
+ 
+      rLog(channel_, "completed writing %s", name.c_str());
+ 
+      auto fileSize = fileUtil_->size(name);
+ 
+      descriptor_->add(dest_, name, 
+            totalSeconds, formatSubchunk.samplesPerSecond, formatSubchunk.channels,
+            fileSize);
+ 
+      //out.close();
+   }
+
    uint32_t dataLength(
          uint32_t samples,
          uint32_t bytesPerSample,
          uint32_t channels
          ) const {
       return samples * bytesPerSample * channels;
+   }
+
+   void writeSamples(ostream* out, char* data,
+          uint32_t startingSample,
+          uint32_t samplesToWrite,
+          uint32_t bytesPerSample,
+          uint32_t channels = 1) {
+      rLog(channel_, "writing %u samples", samplesToWrite);
+ 
+      for (auto sample = startingSample; 
+           sample < startingSample + samplesToWrite; 
+           sample++) {
+         auto byteOffsetForSample = sample * bytesPerSample * channels;
+ 
+         for (uint32_t channel{0}; channel < channels; channel++) {
+            auto byteOffsetForChannel =
+               byteOffsetForSample + (channel * bytesPerSample);
+            for (uint32_t byte{0}; byte < bytesPerSample; byte++) 
+               out->put(data[byteOffsetForChannel + byte]);
+         }
+      }
    }
 
 private:
