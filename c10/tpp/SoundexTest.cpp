107c1eb208e07aff63d9c2b5fc854c558b216328
pswcw <swpeng0831@gmail.com>
2020-05-06 18:42:24 +0800

    [chapter 10] tpp 4: add test

--- c10/tpp/SoundexTest.cpp
+++ c10/tpp/SoundexTest.cpp
@@ -10,3 +10,7 @@ TEST(ASoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    CHECK_EQUAL("A000", soundex.encode("A"));
    CHECK_EQUAL("B000", soundex.encode("B"));
 }
+
+TEST(ASoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+   CHECK_EQUAL("A100", soundex.encode("Ab"));
+}
