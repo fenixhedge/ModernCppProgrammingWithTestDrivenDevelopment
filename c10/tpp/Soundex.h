78382ace7067e86a91cab5a636c79946889c62dc
pswcw <swpeng0831@gmail.com>
2020-05-06 18:47:32 +0800

    [chapter 10] tpp 5: refactor

--- c10/tpp/Soundex.h
+++ c10/tpp/Soundex.h
@@ -7,12 +7,23 @@ class Soundex {
 public:
    std::string encode(const std::string& word) const {
       std::string code("");
-      code += word[0];
-      if (word[1])
-         code += "100";
-      else
-         code += "000";
-      return code;
+      code += head(word) + encodeTail(word);
+      return zeroPad(code);
+   }
+
+   char head(const std::string& word) const {
+      return word[0];
+   }
+
+   std::string encodeTail(const std::string& word) const {
+      if (word[1] == 0) return "";
+      return "1";
+   }
+
+   std::string zeroPad(const std::string& code) const {
+      if (code[1] != 0)
+         return code + "00";
+      return code + "000";
    }
 };
 #endif
